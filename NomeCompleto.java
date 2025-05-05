7bdbb98b1793351d64cc1cec277de946975c8d03
Ronaldo Souza <rona.castro0812@gmail.com>
2025-05-05 09:16:21 -0300

    Add files via upload

---
+++ NomeCompleto.java
@@ -0,0 +1,27 @@
+
+package nomecompleto;
+import java.util.Scanner;
+        
+public class NomeCompleto {
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String nome, sobrenome, nomeCompleto;
+
+        System.out.print("Digite o nome: ");
+        nome = scanner.nextLine();
+
+        System.out.print("Digite o sobrenome: ");
+        sobrenome = scanner.nextLine();
+
+        nomeCompleto = nome + " " + sobrenome;
+
+        System.out.println("Nome completo: " + nomeCompleto);
+
+        scanner.close();
+    }
+}
+
+    
+    
+
