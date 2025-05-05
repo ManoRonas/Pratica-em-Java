4a07e4ad42190cc4ea1ed18c4a529dba6c16b0d8
Ronaldo Souza <rona.castro0812@gmail.com>
2025-05-05 08:20:39 -0300

    Add files via upload

---
+++ vetores]/src/vetores/Vetores.java
@@ -0,0 +1,42 @@
+
+package vetores;
+
+import java.util.Scanner;
+
+public class Vetores {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int tamanho = 5;
+        int[] a = new int[tamanho];
+        int[] b = new int[tamanho];
+
+        System.out.println("Digite " + tamanho + " valores para o vetor A:");
+
+        
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print("A[" + i + "] = ");
+            a[i] = scanner.nextInt();
+        }
+
+       
+        for (int i = 0; i < tamanho; i++) {
+            b[i] = a[i];
+        }
+
+        
+        System.out.println("\nVetor A:");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+        System.out.println("\nVetor B (cópia de A):");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print(b[i] + " ");
+        }
+
+        scanner.close();
+    }
+}
+
+
