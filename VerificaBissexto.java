8af185a014aac3d6b62c5201bea31ffe9846421f
Ronaldo Souza <rona.castro0812@gmail.com>
2025-05-12 21:05:10 -0300

    Add files via upload

---
+++ VerificaBissexto.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//Questão 4 Funçoes
+
+public class VerificaBissexto {
+
+    // Função que verifica se o ano é bissexto
+    public static boolean ehBissexto(int ano) {
+        return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um ano: ");
+        int ano = scanner.nextInt();
+
+        if (ehBissexto(ano)) {
+            System.out.println(ano + " é um ano bissexto.");
+        } else {
+            System.out.println(ano + " não é um ano bissexto.");
+        }
+
+        scanner.close();
+    }
+}
